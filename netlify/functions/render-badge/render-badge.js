3c2cdd284eabb8526f2134a81ea4283937fbbae1
Maxim Salnikov <salnikov@gmail.com>
2021-07-29 20:29:15 +0200

    promisified, dotenv restored

--- netlify/functions/render-badge/render-badge.js
+++ netlify/functions/render-badge/render-badge.js
@@ -1,4 +1,20 @@
 const handler = async (event) => {
+  /**
+   * Renders image in Cloudinary
+   * @param {object} providerInstance Cloudinary instance
+   * @param {string} baseImagePath Background image path
+   * @param {object} overlayConfig Overlay configuration
+   * @return {string} Generated image url
+   */
+  function uploadImage(providerInstance, baseImagePath, overlayConfig) {
+    return new Promise((resolve, reject) => {
+      providerInstance.uploader.upload(baseImagePath, overlayConfig, (error, result) => {
+        if (error) return reject(error);
+        return resolve(result);
+      });
+    });
+  }
+
   try {
     if (!event.body) {
       return {
@@ -33,6 +49,9 @@ const handler = async (event) => {
 
     const eventName = 'pwasummit';
     const eventYear = '2021';
+    const baseImagePath = 'netlify/functions/render-badge/assets/badge-bg.png';
+
+    if (!process.env.NETLIFY) require('dotenv').config();
 
     cloudinary.config({
       cloud_name: eventName,
@@ -40,57 +59,52 @@ const handler = async (event) => {
       api_secret: process.env.CLOUDINARY_SECRET,
     });
 
+    const overlayConfig = {
+      folder: `${eventName}/${eventYear}`,
+      unique_filename: false,
+      filename_override: ticketId,
+      public_id: ticketId,
+      overwrite: true,
+      access_mode: 'public',
+      invalidate: true,
+      tags: [eventName, eventYear, [firstName, lastName].join(' '), ticketId, ticketType],
+      transformation: [
+        { width: 1200 },
+        {
+          color: '#FFFFFF',
+          overlay: {
+            font_family: 'Work Sans',
+            font_size: firstName.length > 11 || lastName.length > 11 ? 63 : 73,
+            font_weight: 'medium',
+            text: [firstName, lastName].join(' ').toUpperCase(),
+            text_align: 'center',
+          },
+          width: 650,
+          crop: 'fit',
+        },
+        { flags: 'layer_apply', gravity: 'north', y: 0.47 },
+        {
+          color: '#FFFFFF',
+          overlay: {
+            font_family: 'Work Sans',
+            font_size: 50,
+            font_weight: 'regular',
+            text: ticketType,
+            text_align: 'center',
+          },
+        },
+        { flags: 'relative', width: '0.3', crop: 'limit' },
+        { flags: 'layer_apply', gravity: 'north', y: 0.63 },
+      ],
+    };
 
-    await cloudinary.uploader.upload(
-      'netlify/functions/render-badge/assets/badge-bg.png',
-      {
-        folder: `${eventName}/${eventYear}`,
-        unique_filename: false,
-        filename_override: ticketId,
-        public_id: ticketId,
-        overwrite: true,
-        access_mode: 'public',
-        invalidate: true,
-        tags: [eventName, eventYear, [firstName, lastName].join(' '), ticketId, ticketType],
-        transformation: [
-          { width: 1200 },
-          {
-            color: '#FFFFFF',
-            overlay: {
-              font_family: 'Work Sans',
-              font_size: firstName.length > 11 || lastName.length > 11 ? 63 : 73,
-              font_weight: 'medium',
-              text: [firstName, lastName].join(' ').toUpperCase(),
-              text_align: 'center',
-            },
-            width: 650,
-            crop: 'fit',
-          },
-          { flags: 'layer_apply', gravity: 'north', y: 0.47 },
-          {
-            color: '#FFFFFF',
-            overlay: {
-              font_family: 'Work Sans',
-              font_size: 50,
-              font_weight: 'regular',
-              text: ticketType,
-              text_align: 'center',
-            },
-          },
-          { flags: 'relative', width: '0.3', crop: 'limit' },
-          { flags: 'layer_apply', gravity: 'north', y: 0.63 },
-        ],
-      },
-      (error, result) => {
-        console.error(error, result);
-      },
-    );
+    const result = await uploadImage(cloudinary, baseImagePath, overlayConfig);
+
     return {
       statusCode: 200,
-      body: JSON.stringify({ result: 'Render request was sent' }),
+      body: result.secure_url,
     };
   } catch (error) {
-    console.error(error.toString());
     return {
       statusCode: 500,
       body: JSON.stringify({
